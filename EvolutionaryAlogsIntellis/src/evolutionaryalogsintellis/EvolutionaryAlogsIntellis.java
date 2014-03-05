14960deb4ccf1fed19881dde9e2fd584008be8be
Gia Sadhwani <giamail@gmail.com>
2014-03-05 17:49:29 +0000

    Just a proxy test with proxy classes

---
+++ EvolutionaryAlogsIntellis/src/evolutionaryalogsintellis/EvolutionaryAlogsIntellis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package evolutionaryalogsintellis;
+
+/**
+ *
+ * @author Gia
+ */
+public class EvolutionaryAlogsIntellis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // adding a comment here by gia
+    }
+    
+}
