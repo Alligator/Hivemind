eb4f13a4f8337ad7e4abd8613349a73b449c1bb3
Gia Sadhwani <giamail@gmail.com>
2014-03-05 18:18:15 +0000

    First Implementation of Fitness Class

--- src/hivemind/Hivemind.java
+++ src/hivemind/Hivemind.java
@@ -18,7 +18,39 @@ public class Hivemind {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // gia made a comment here
+        //-------------    Fitness Class     ----------------------
+        
+        String filePath = "C:/Users/Gia/Desktop/IntelliSysAss2/datfile.dat";
+        boolean print = false;
+        double a = 3.0;
+        double b = 0.5432;
+        double c = 0.44234;
+        double d = 0.64756;
+        double e = 0.76574;
+        double f = 0.767;
+        double testX = 450.0;
+        
+        Fitness fitness = new Fitness();
+        
+        double YfromX = fitness.getYValFromFunctionX(a,b,c,d,e,f,testX,print);
+        System.out.println("Y value from X in function is   " + YfromX);
+        
+        fitness.printHashMap(578, print); // sample data points as static?????
+        
+        double fitnessVal = fitness.calculateCurveFitness(a,b,c,d,e,f,print);
+        System.out.println(fitnessVal);
+        
+        
+        String gnuCmd = fitness.printPlotGNUCommands(filePath, 0,4.3,3,2.3,0.5,0);
+        System.out.println(gnuCmd);
+        
+        
+        //------------------ Population Class -----------------//
+        
+        Population pop = new Population();
+        
+        //print = true;
+        //double[][] currentPopulation = pop.getPopulation(print);
     }
     
 }
