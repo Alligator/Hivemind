aa181154b4ff8f1853c72079e704c4bdb249004c
End-S <r.raynsford@gmail.com>
2014-03-06 23:20:32 +0000

    Individual class near completion

--- src/hivemind/Individual.java
+++ src/hivemind/Individual.java
@@ -6,6 +6,10 @@
 
 package hivemind;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  *
  * @author Gia
@@ -13,35 +17,100 @@ package hivemind;
 public class Individual {
     private double[] coefficients;
     private double fitness;
-
-    public Individual() {
-        
-        
-        
+    private int rangeMax, rangeMin;
+    private Random rand = new Random();
+    
+    /**
+     * Constructor for when creating a fresh Individual
+     * @param rangeMin the minimum range
+     * @param rangeMax the maximum range
+     */
+    public Individual(int rangeMin, int rangeMax) {
+        this.rangeMax=rangeMax;
+        this.rangeMin=rangeMin;
+        for(int i=0;i<coefficients.length;i++){
+            coefficients[i]=rangeMin+rand.nextDouble()*((rangeMax-rangeMin));
+        }
     }
     
-
-
-    public double getFitness() {
-        return fitness;
+    /**
+     * Constructor for sending in an array of coefficients
+     * @param coefficients from a new population
+     */
+    public Individual(double[] coefficients){
+        this.coefficients=coefficients;
     }
     
-    public double setFitness(double fitness){
-        
+     /**
+     * set the fitness of this individual
+     * @param fitness pre calculated for this individual
+     * @return 
+     */
+    public double setFitness(double fitness){       
         this.fitness = fitness;
         return fitness;
     }
     
+    /**
+     * returns the fitness of this individual
+     * @return fitness
+     */
+    public double getFitness() {
+        return fitness;
+    }   
+    
+    /**
+     * returns this individual
+     * @return the coefficients of this individual
+     */
+    public double[] getIndividual(){
+        return coefficients;
+    }
+    
+    /**
+     * sets the coefficients of this individual to a new set
+     * @param coefficients new set of coefficients
+     * @return the new coefficients;
+     */
     public double[] setCoefficients(double[] coefficients){
         this.coefficients = coefficients;
         return coefficients;
     }
     
+    /**
+     * returns this individuals coefficients
+     * @return coefficients
+     */
     public double[] getCoefficients(){
         return coefficients;
     }
     
+    /**
+     * picks random number in coefficient array and mutates it 
+     * @param coefficients array
+     */
     public void mutate(){
-        //here be mutants
+        int i = rand.nextInt(coefficients.length);
+        //not sure if this is 0-1 or 0-0.999999... Might need to +1
+        coefficients[i]=rangeMin+rand.nextDouble()*((rangeMax-rangeMin));
+        System.out.println(coefficients[i]);
+    }
+    
+     /**
+     * picks random number in coefficient array and mutates it 
+     * @param coefficients array
+     */
+    public void multiMutate(){
+        List<Integer> record = new ArrayList<>();
+        int i;
+        int mutateQuantity = rand.nextInt(coefficients.length);
+        for(int j=0; j<=mutateQuantity;){
+            i = rand.nextInt(coefficients.length);
+            if(!record.contains(i)){
+                coefficients[i]=rangeMin+rand.nextDouble()*((rangeMax-rangeMin));
+                record.add(i);
+                j++;
+            }
+        }
     }
 }
